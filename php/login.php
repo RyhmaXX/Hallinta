adb9ab93eac2709fb8fe27bf06d74910f975e63e
Kallio95 <kallio95@gmail.com>
2017-10-31 11:56:03 +0200

    Add files via upload

--- php/login.php
+++ php/login.php
@@ -1,70 +1,71 @@
 <?php
 
-session_start();
+	session_start();
+	session_unset();
 
-try {
-	
-	include("db.inc");
-
-	$postdata = file_get_contents("php://input");
-	$request = json_decode($postdata);
-
-	$kt = $request->kt;
-	$pw = $request->salasana;
-
-	$success = false;
-
-	if ($kt != "" && $pw != "") {
-		$query = $conn->prepare("SELECT * 
-								FROM admin
-								WHERE username=?");
-		$query->bind_param("s", $kt);
-		$query->execute();
+	try {
 		
-		$result = $query->get_result();
-		
-		if (mysqli_num_rows($result) != 1) {
-			echo "Väärä määrä rivejä!";
-		} else {
-			$row = $result->fetch_assoc();
+		include("db.inc");
+
+		$postdata = file_get_contents("php://input");
+		$request = json_decode($postdata);
+
+		$kt = $request->kt;
+		$pw = $request->salasana;
+
+		$success = false;
+
+		if ($kt != "" && $pw != "") {
+			$query = $conn->prepare("SELECT * 
+									FROM admin
+									WHERE username=?");
+			$query->bind_param("s", $kt);
+			$query->execute();
 			
-			$id = $row["id"];
-			$domain = $row["domain_id"];
-			$pwfromdb = $row["password"];
+			$result = $query->get_result();
 			
-			if ($pwfromdb == $pw) {
+			if (mysqli_num_rows($result) != 1) {
+				echo "Väärä määrä rivejä!";
+			} else {
+				$row = $result->fetch_assoc();
 				
-				$_SESSION["user"] = [
-					"id" => $id,
-					"username" => $kt,
-					"domain" => $domain
-				];
+				$id = $row["id"];
+				$domain = $row["domain_id"];
+				$pwfromdb = $row["password"];
 				
-				$success = true;
+				if ($pwfromdb == $pw) {
+					
+					$_SESSION["user"] = [
+						"id" => $id,
+						"username" => $kt,
+						"domain" => $domain
+					];
+					
+					$success = true;
+				}
 			}
 		}
+
+		$resp = [];
+
+		if ($success) {
+
+			$resp["code"] = 0;
+			$resp["domain"] = $domain;
+			$resp["username"] = $kt;
+			
+		} else {
+			
+			$resp["code"] = 1;
+			
+		}
+
+	} catch (Exception $e) {
+		// Unknown php-error
+		$resp = [];
+		$resp["code"] = -1;
 	}
 
-	$resp = [];
-
-	if ($success) {
-
-		$resp["code"] = 0;
-		$resp["domain"] = $domain;
-		$resp["username"] = $kt;
-		
-	} else {
-		
-		$resp["code"] = 1;
-		
-	}
-
-} catch (Exception $e) {
-	// Unknown php-error
-	$resp = [];
-	$resp["code"] = -1;
-}
-
-echo json_encode($resp);
+	echo json_encode($resp);
 
 ?>
