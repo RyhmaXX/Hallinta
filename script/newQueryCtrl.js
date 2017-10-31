b3b37be1fe26c969dc881b6ea99cc78dca4ae775
Teemu Taavitsainen <32508036+Spudez@users.noreply.github.com>
2017-10-31 15:54:35 +0200

    Add files via upload

--- script/newQueryCtrl.js
+++ script/newQueryCtrl.js
@@ -4,23 +4,71 @@ app.controller("newQueryCtrl", function ($scope, $window, $http, $location){
 	$scope.startdate = new Date();
 	$scope.enddate = new Date();
 	$scope.skipdate = false;
+	$scope.skiptemplate = false;
+	$scope.selectedtemplate = "";
+	$scope.selectedUsers = "";
+	$scope.selectedLake = "";
+	
+	$http.get("php/getLakes.php").then(function(response){
+		if (response.data.code == 0) {
+			$scope.lakes = response.data.lakes;
+		} else if (response.data.code == 1) {
+			$location.path("/new");
+		} else {
+			alert("error: " + response.data.code);
+		}
+	});
+	
+	$http.get("php/getUsersByAreas.php").then(function(response){
+		if (response.data.code == 0) {
+			$scope.users = response.data.users;
+		} else if (response.data.code == 1) {
+			$location.path("/new");
+		} else {
+			alert("error: " + response.data.code);
+		}
+	});
+	
+	$http.get("php/getTemplates.php").then(function(response){
+		if (response.data.code == 0) {
+			$scope.templates = response.data.templates;
+		} else if (response.data.code == 1) {
+			$location.path("/new");
+		} else {
+			alert("error: " + response.data.code);
+		}
+	});
+	
+	/* Testi funktio
+	$scope.testi = function() {
+
+				alert($scope.selectedLake);
+				
+		};
+	*/
 	
 	$scope.saveEventInfo = function() {
 		if ($scope.skipdate == false){
-			var data = {
-				'name' : $scope.name,
-				'startdate' : Math.floor($scope.startdate.getTime() / 1000),
-				'enddate' : Math.floor($scope.enddate.getTime() / 1000),
-				'status' : 1
-			};
+			if ($scope.selectedtemplate != "---Ei pohjaa---" && $scope.selectedtemplate != ""){
+				var data = {
+					'name' : $scope.name,
+					'startdate' : Math.floor($scope.startdate.getTime() / 1000),
+					'enddate' : Math.floor($scope.enddate.getTime() / 1000),
+					'status' : 1,
+					'selectedtemplate' : $scope.selectedtemplate
+				};
+			}
 		}
 		else {
-			var data = {
-				'name' : $scope.name,
-				'startdate' : null,
-				'enddate' : null,
-				'status' : 1
-			};
+			if ($scope.selectedtemplate == "---Ei pohjaa---"){
+				var data = {
+					'name' : $scope.name,
+					'startdate' : null,
+					'enddate' : null,
+					'status' : 1
+					
+				};
+			}
 		}
 		
 		$http.post("php/setPoll.php", data).then(function(response){
