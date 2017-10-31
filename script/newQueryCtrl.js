a3c37c1c7c0a0c6c83aa27b21997ab846a3932fa
Spudez <spude.email@gmail.com>
2017-10-31 14:56:59 +0200

    Kysymyspohjien haku/valinta
    newQueryCtrl hakee nyt kysymysten pohjat kannasta ja liittää ne dropdown valikkoon newQuery.html tiedostossa.

--- script/newQueryCtrl.js
+++ script/newQueryCtrl.js
@@ -4,23 +4,49 @@ app.controller("newQueryCtrl", function ($scope, $window, $http, $location){
 	$scope.startdate = new Date();
 	$scope.enddate = new Date();
 	$scope.skipdate = false;
+	$scope.skiptemplate = false;
+	$scope.selectedtemplate = "";
+	
+	$http.get("php/getTemplates.php").then(function(response){
+		if (response.data.code == 0) {
+			$scope.templates = response.data.templates;
+		} else if (response.data.code == 1) {
+			$location.path("/new");
+		} else {
+			alert("error: " + response.data.code);
+		}
+	});
+	
+	/* Testi funktio jolla voi tarkastaa lähetettävä template
+	$scope.testi = function() {
+		if ($scope.selectedtemplate != "Ei pohjaa" && $scope.selectedtemplate != ""){
+				alert($scope.selectedtemplate);
+				}
+		};
+	*/	
 	
 	$scope.saveEventInfo = function() {
 		if ($scope.skipdate == false){
-			var data = {
-				'name' : $scope.name,
-				'startdate' : Math.floor($scope.startdate.getTime() / 1000),
-				'enddate' : Math.floor($scope.enddate.getTime() / 1000),
-				'status' : 1
-			};
+			if ($scope.selectedtemplate != "Ei pohjaa" && $scope.selectedtemplate != ""){
+				var data = {
+					'name' : $scope.name,
+					'startdate' : Math.floor($scope.startdate.getTime() / 1000),
+					'enddate' : Math.floor($scope.enddate.getTime() / 1000),
+					'status' : 1,
+					'selectedtemplate' : $scope.selectedtemplate
+				};
+			}
 		}
 		else {
-			var data = {
-				'name' : $scope.name,
-				'startdate' : null,
-				'enddate' : null,
-				'status' : 1
-			};
+			if ($scope.selectedtemplate == "Ei pohjaa"){
+				var data = {
+					'name' : $scope.name,
+					'startdate' : null,
+					'enddate' : null,
+					'status' : 1
+					
+				};
+			}
 		}
 		
 		$http.post("php/setPoll.php", data).then(function(response){
