70f2c2f86818ba4aa55ce993cc02e76700441b14
Teemu Taavitsainen <32508036+Spudez@users.noreply.github.com>
2017-11-08 12:04:58 +0200

    Add files via upload

--- script/homeCtrl.js
+++ script/homeCtrl.js
@@ -1,5 +1,7 @@
 app.controller("homeCtrl", function ($scope, $window, $http, $location){
 	
+	$scope.id = 0;
+	
 	$http.get("php/getPolls.php").then(function(response){
 		if (response.data.code == 0) {
 			$scope.polls = response.data.polls;
@@ -9,4 +11,28 @@ app.controller("homeCtrl", function ($scope, $window, $http, $location){
 			alert("error");
 		}
 	});
+	
+	$scope.saveId = function(id) {
+		
+			$scope.id = id;
+		
+	}
+	
+	$scope.publish = function() {
+		
+			if ($scope.id > 0){
+				$http.post("php/publishPoll.php").then(function(response){
+					if (response.data.code == 0) {
+						alert("Kysely on julkaistu");
+					}
+					else {
+						alert("error" + response.data.code);
+					}
+					$('#myModal').modal('hide');
+				});
+			}
+			
+			
+		
+	}
 });
