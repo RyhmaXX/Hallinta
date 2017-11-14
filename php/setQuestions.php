d1a6b927bbafefaef9a89117b7cea7c81aa21ead
Kallio95 <kallio95@gmail.com>
2017-11-14 14:32:27 +0200

    Add files via upload

--- php/setQuestions.php
+++ php/setQuestions.php
@@ -2,9 +2,96 @@
 	
 	session_start();
 	
+	function setChoices($conn, $poll, $choices, $qnum) {
+		
+		$errors = 0;
+		
+		$query = $conn->prepare("INSERT INTO choice (num, name, question_poll_id, question_question_num)
+								VALUES (?, ?, ?, ?)");
+		
+		foreach ($choices as $choise) {
+			
+			$query->bind_param("isii", $choise["num"], $choise["name"], $poll, $qnum);
+			
+			if ($query->execute()) {
+				// ok
+			} else {
+				$errors++;
+			}
+		}
+		
+		return $errors;
+	}
+	
+	function setRows($conn, $mid, $rows) {
+		
+		$errors = 0;
+		
+		$query = $conn->prepare("INSERT INTO qm_row (qm_id, num, title)
+								VALUES (?, ?, ?)");
+		
+		foreach ($rows as $row) {
+			
+			$query->bind_param("iis", $mid, $row["num"], $row["title"]);
+			
+			if ($query->execute()) {
+				// ok
+			} else {
+				$errors++;
+			}
+		}
+		
+		return $errors;
+	}
+	
+	function setColumns($conn, $mid, $cols) {
+				
+		$errors = 0;
+		
+		$query = $conn->prepare("INSERT INTO qm_column (qm_id, num, title, type)
+								VALUES (?, ?, ?, ?)");
+		
+		foreach ($cols as $col) {
+			
+			$query->bind_param("iisi", $mid, $col["num"], $col["title"], $col["type"]);
+			
+			if ($query->execute()) {
+				// ok
+			} else {
+				$errors++;
+			}
+		}
+		
+		return $errors;
+	}
+	
+	function setMatrix($conn, $poll, $matrix, $qnum) {
+		
+		$errors = 0;
+		
+		$query = $conn->prepare("INSERT INTO question_matrix (poll_id, question_num, header)
+								VALUES (?, ?, ?)");
+								
+		$query->bind_param("iis", $poll, $qnum, $matrix["header"]);
+		
+		if ($query->execute()) {
+			$mid = $conn->insert_id;
+			
+			$errors += setRows($conn, $mid, $matrix["rows"]);
+			$errors += setColumns($conn, $mid, $matrix["columns"]);
+			
+		} else {
+			$errors++;
+		}
+		
+		return $errors;
+	}
+	
 	try {
 		
 		include("db.inc");
+		
+		$errors = 0;
 		
 		$resp = [];
 		
@@ -19,21 +106,34 @@
 			$query = $conn->prepare("INSERT INTO question (question_num, poll_id, question, question_types_id)
 									VALUES (?, ?, ?, ?)");
 			
-			$num = 1;
-			
 			foreach ($questions as $question) {
 				$query->bind_param("iisi", $question[0], $poll, $question[1], $question[2]);
 				if ($query->execute()) {
 					// Success
-					$resp["code"] = 0;
+					
+					if ($type > 99 && $type < 200) {
+						$errors += setChoices($conn, $poll, $question[3], $question[0]);
+						
+						$query = $conn->prepare("INSERT INTO question (question_num, poll_id, question, question_types_id)
+									VALUES (?, ?, ?, ?)");
+					} else if ($type > 199) {
+						$errors += setMatrix($conn, $poll, $question[3], $question[0]);
+						
+						$query = $conn->prepare("INSERT INTO question (question_num, poll_id, question, question_types_id)
+									VALUES (?, ?, ?, ?)");
+					}
+					
+					
+
 				} else {
 					// Error
-					$resp["code"] = -1;
+					$errors++;
 				}
-				$num++;
+				
 			}
 			
-			
+			$resp["code"] = 0;
+			$resp["errors"] = $errors;
 			
 		} else {
 			// Not logget in
