3ed593bb2be3e6db14b7fc364876d02356385ec6
Kallio95 <kallio95@gmail.com>
2017-11-22 12:14:24 +0200

    Add files via upload

---
+++ php/deletePoll.php
@@ -0,0 +1,38 @@
+<?php
+	session_start();
+	
+	try {
+		
+		include("db.inc");
+		
+		if (isset($_SESSION["user"])) {
+			
+			$resp = [];
+			
+			$postdate = file_get_contents("php://input");
+			$request = json_decode($postdata);
+			$id = $request->id;
+			
+			$domain = $_SESSION["user"]["domain"];
+			
+			$query = $conn->prepare("DELETE FROM poll
+									WHERE domain = ? AND id = ?");
+									
+			$query->bind_param("ii", $domain, $id);
+			
+			if ($query->execute()) {
+				$resp["code"] = 0;
+			} else {
+				$resp["code"] = 2;
+			}
+		}
+		
+	} catch(Exception $e) {
+		
+		$resp = [];
+		$resp["code"] = -1;
+		
+	}
+	
+	echo json_encode($resp);
+?>
