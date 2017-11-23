b194cbe1ba8b8040c93b233beede58574d603cce
Kallio95 <kallio95@gmail.com>
2017-11-23 17:57:23 +0200

    Update deletePoll.php

--- php/deletePoll.php
+++ php/deletePoll.php
@@ -9,7 +9,7 @@
 			
 			$resp = [];
 			
-			$postdate = file_get_contents("php://input");
+			$postdata = file_get_contents("php://input");
 			$request = json_decode($postdata);
 			$id = $request->id;
 			
